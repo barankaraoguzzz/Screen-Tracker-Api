e904d969d7229b54b853cf89cbda7a09d741821b
Baran Karaoğuz <b.b.karaoguz@gmail.com>
2025-06-15 01:13:17 +0300

    update "start.sh" file

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Default port if PORT environment variable is not set
-export PORT=${PORT:-8000}
-
-# Start the application
-exec uvicorn app.main:app --host 0.0.0.0 --port $PORT --proxy-headers
+# Uvicorn'u doğrudan başlat, Railway zaten $PORT'u sağlam veriyor
+exec uvicorn app.main:app --host 0.0.0.0 --port "$PORT" --proxy-headers
